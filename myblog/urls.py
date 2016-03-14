9eab564ffa7f298c8da28f9c2e1bee15834f51b4
gchandel6 <gchandel6@gmail.com>
2016-03-14 08:00:40 +0530

    Added view and template for a particular post

--- myblog/urls.py
+++ myblog/urls.py
@@ -4,6 +4,13 @@ from . import views
 
 urlpatterns = [
 
+#   URL for Home Page
+
     url(r'^$' , views.post_list , name='post_list'),
+
+
+#   URL for A Post's Page
+
+    url(r'^post/(?P<post_id>\d+)/$' , views.post_page, name = 'post_page' ),
 
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+
+from django.shortcuts import render , get_object_or_404
 from django.utils import timezone
 from .models import newpost
 
 # Create your views here.
 
 
+
+#  View for  Home page
+
 def post_list(request):
 
     posts = newpost.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     return render(request , 'myblog/post_list.html', {'posts':posts  })
+
+
+#   View for A Particular post
+
+def post_page(request , post_id):
+
+    post = get_object_or_404(newpost , pk=post_id);
+
+    return render(request, 'myblog/post_page.html' , { 'post' : post } );
